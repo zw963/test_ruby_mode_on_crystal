a4f6448481119fa95b7f589f310b2a453aa17369
Billy.Zheng <vil963@gmail.com>
2022-07-04 22:18:49 +0800

    Initial commit

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/test_ruby_mode_on_crystal"
---
+++ src/test_ruby_mode_on_crystal.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `TestRubyModeOnCrystal`
+module TestRubyModeOnCrystal
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
